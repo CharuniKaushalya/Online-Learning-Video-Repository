97d6eb9d011e5b29f649cc8f9002346fac559ffd
Charuni Kaushalya <snkaushi@gmail.com>
2017-02-19 19:48:24 +0530

    Add front end

---
+++ Code/web/app/views/videos/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Videos */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="videos-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'createDate')->textInput() ?>
+
+    <?= $form->field($model, 'creator')->textInput() ?>
+
+    <?= $form->field($model, 'rating')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ Code/web/app/views/videos/index.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\VideosSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Videos';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="videos-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Videos', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'title',
+            'url:url',
+            'description:ntext',
+            'createDate',
+            // 'creator',
+            // 'rating',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
